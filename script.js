3a19233a3a0abe0333634a2a1c7be30e8991cd78
John Memmott <jasonmemm@gmail.com>
2021-09-30 10:52:09 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//? Named Function
+// create two variables that each hold a number
+// create a function that accepts to numbers, adds them together, and returns the value
+// use variables made above as arguments to run the function
+// store the value returned in a variable and console.log that variable
+
+//? Anonymous Function
+// create two variables that each hold a string
+// create an anonymous function that accepts to strings, puts them together into one string, and returns the value
+// store the function in a variable
+// use variables made above as arguments to run the function with the newly-created function variable
+// store the value returned in a variable and console.log that variable
